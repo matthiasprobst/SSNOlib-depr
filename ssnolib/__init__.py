bc198f3616c6abdb0e66b9d135a7a6a32bc9738d
Matthias Probst <matth.probst@gmail.com>
2024-01-18 11:30:25 +0100

    update package meta

--- ssnolib/__init__.py
+++ ssnolib/__init__.py
@@ -1,1 +1,5 @@
+from ._version import __version__
+
 from .classes import StandardNameTable, StandardName, Distribution, Contact
+
+__all__ = ('__version__', 'StandardNameTable', 'StandardName', 'Distribution', 'Contact')
